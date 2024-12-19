aec1b97b2458da45b44483302871cfc5ed311cf8
Kiran Saleem <saleemkiran1599@gmail.com>
2024-12-19 23:51:41 +0500

    first Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'myApp.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.amberAccent,
+        scaffoldBackgroundColor: Colors.blue.shade100,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.indigo,
+        ),
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.indigo,
+            centerTitle: true,
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 24)),
+        textTheme: TextTheme(
+            headlineLarge: TextStyle(fontSize: 24, color: Colors.white),
+            headlineMedium: TextStyle(
+                fontSize: 22,
+                color: Colors.indigo,
+                fontWeight: FontWeight.bold)),
+      ),
+      home: GuessCountry(),
+    );
+  }
+}
